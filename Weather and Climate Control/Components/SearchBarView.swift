83234b676a847f51fdbb095ddc90c4e31cb28924
Yumeng Liu <lym20041026@gmail.com>
2024-07-07 08:15:08 -0700

    Added new file

---
+++ Weather and Climate Control/Components/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarView.swift
+//  Weather
+//
+//  Created by Yumeng Liu on 7/7/24.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
